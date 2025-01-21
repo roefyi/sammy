29f846bc06f24a0d0882f0c6ec2f4c297ead4e40
roefyi <romansdenson@gmail.com>
2025-01-20 22:49:48 -0600

    Initial Commit

---
+++ sammy/sammyApp.swift
@@ -0,0 +1,17 @@
+//
+//  sammyApp.swift
+//  sammy
+//
+//  Created by Rome on 1/20/25.
+//
+
+import SwiftUI
+
+@main
+struct sammyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
